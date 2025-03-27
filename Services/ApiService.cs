eff1184a3fd8455f52277caea8a9b878b4555291
João Brito <83669513+JaoBrito@users.noreply.github.com>
2025-03-27 16:20:17 -0300

    frontend finalizado

--- Services/ApiService.cs
+++ Services/ApiService.cs
@@ -12,9 +12,38 @@ namespace ProjetoLeads.Frontend.Services
             _httpClient = httpClient;
         }
 
-        public async Task<List<LeadModel>> GetDadosAsync( )
+        public async Task<List<LeadModel>> GetLeadAsync( )
         {
             return await _httpClient.GetFromJsonAsync<List<LeadModel>>( "api/leads" );
+        }
+
+        public async Task<LeadModel> GetLeadByIdAsync( int id )
+        {
+            return await _httpClient.GetFromJsonAsync<LeadModel>( $"api/leads/{id}" );
+        }
+
+        public async Task<LeadModel> CreateLeadAsync( LeadModel lead )
+        {
+            var response = await _httpClient.PostAsJsonAsync( "api/leads", lead );
+
+            if ( response.IsSuccessStatusCode )
+            {
+                return await response.Content.ReadFromJsonAsync<LeadModel>();
+            }
+
+            return null;
+        }
+
+        public async Task<bool> UpdateLeadAsync( int id, LeadModel lead )
+        {
+            var response = await _httpClient.PutAsJsonAsync( $"api/leads/{id}", lead );
+            return response.IsSuccessStatusCode;
+        }
+
+        public async Task<bool> DeleteLeadAsync( int id )
+        {
+            var response = await _httpClient.DeleteAsync( $"api/leads/{id}" );
+            return response.IsSuccessStatusCode;
         }
     }
 }
